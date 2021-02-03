1b89eb9caba04a02b9a100f39e7cd9d6a8c2b8a9
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2021-02-03 11:29:30 -0500

    Create slock.py

---
+++ cogs/slock.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+client = discord.Client()
+
+
+class slock(commands.Cog):
+
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command(aliases=["sl", "slock"])
+    async def serverlock(self, ctx):
+		text_channel_list = []
+		for guild in client.guilds:
+			for channel in guild.text_channels:
+				text_channel_list.append(channel)
+				overwrite = discord.PermissionOverwrite()
+				overwrite.send_messages = False
+				overwrite.read_messages = True
+				await channel.set_permissions(member, overwrite=overwrite)
+
+				
+
+def setup(client):
+    client.add_cog(slock(client))
