70a21c791a9c132474b7905cce5fbf5a0297ee1b
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2021-02-27 08:48:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import discord
 from discord.ext import commands
-import json
-from datetime import date
 
-client = commands.Bot(command_prefix="-")
+client = commands.Bot(command_prefix="!")
 client.remove_command("help")
 
 extensions = {
@@ -11,12 +9,10 @@ extensions = {
 	"cogs.economy",
 	"cogs.channel",
 	"cogs.games",
-	"cogs.help",
+    "cogs.music",
 	"cogs.misc"
 	#guild cog coming soon
 }
-
-today = date.today()
 
 #create convo cog
 
@@ -26,44 +22,11 @@ today = date.today()
 async def on_ready():
 	"""Puck Initilization"""
 	print(f"{client.user} is now online")
-	await client.change_presence(activity=discord.Streaming(name='rip moms electric bill', url='https://www.twitch.tv/savagepatchboy'))
+	await client.change_presence(activity=discord.Streaming(name='Puck goes brrrrrrrr', url='https://www.twitch.tv/savagepatchboy'))
 	for e in extensions:
 		client.load_extension(e)
-		get = e[-1]
-		splicedcogname = e[5:-1] + get
-		print(f"Loaded {splicedcogname} extension on {today}")
-		with open('//home//pi//Desktop//cogs//loadlog.json') as f:
-			load = json.load(f)
-		
-		#make a json key with the date and the data inside the today date key
-		load[f'{today} - {e}'] = f"{e} Loaded successfully on {today}"
-		
-		with open("//home//pi//Desktop//cogs//loadlog.json", 'w') as f:
-			json.dump(load, f, indent = 4, sort_keys=True)
-
-#get the date and display it inside a json key. After this I want to put it into the Api and test out NGROK for hosting.
-#log everything into a json file then display it on the website.
 
 #this just makes the bot ignore the commandnotfound error
-@client.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        return True
-        
-@client.event
-async def on_message_delete(message):
-	print(message.content)
-	await message.channel.send(f"{message.author} deleted: {message.content}")
-	with open("//home//pi//Desktop//cogs//snipe.json") as f:
-		load = json.load(f)
-		
-	load[f"{message.author}"] = f"{message.content}"
-		
-	with open("//home//pi//Desktop//cogs//snipe.json", 'w') as f:
-		json.dump(load, f, indent = 4, sort_keys = True)
-		
-#make it write into a users account in a json file then make a sniper command to snipe the message from the json file
-#create key that has the message author and last deleted messageyhgf
 
 @client.command()
 async def lock(ctx, channel: discord.TextChannel=None):
@@ -75,6 +38,8 @@ async def lock(ctx, channel: discord.TextChannel=None):
 	await channel.set_permissions(role, overwrite=overwrite)
 
 	await ctx.send('Channel locked.')
+
+
 # @client.event
 # async def on_message(message):
 	# if message.content == "-emoji":
@@ -88,6 +53,5 @@ async def lock(ctx, channel: discord.TextChannel=None):
 
 		
 		
-
 
 client.run("token")
